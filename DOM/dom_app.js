2e701f2741a56bd2bad88feef4657157c82a6530
Ihsaan-20 <ahsaansindhi1@gmail.com>
2024-03-11 16:08:00 +0500

    "main"

---
+++ DOM/dom_app.js
@@ -0,0 +1,52 @@
+// console.log(window);
+// console.dir(window);
+
+
+// document.getElementById('id_here'); 
+// document.getElementsByClassName('class_here');
+// document.getElementsByTagName('tag_here');
+// document.querySelector(".class_here || #id_here || tag_name"); return elements || first element
+// document.querySelectorAll(".class_here || #id_here || tag_name"); return a nodeList || all elements
+
+//tagName
+//innerText
+//innerHTML
+//innerContext
+
+
+//parent
+//children
+//sibling
+
+//firstChild
+//lastChild
+
+//appendChild
+//append
+
+//lastElementChild
+
+let add = document.querySelector("#add");
+let remove = document.querySelector("#remove");
+let container = document.querySelector(".container");
+let index = 0;
+
+add.addEventListener("click", () => {
+    index++;
+    let div = document.createElement("div");
+    div.innerHTML = `<h2>${index}</h2>`;
+    container.appendChild(div);
+    console.log('created');
+});
+
+remove.addEventListener("click", () => {
+    let div = container.lastElementChild;
+    if (div) {
+        container.removeChild(div);
+        console.log('removed');
+    } else {
+        console.log('Nothing to remove');
+    }
+});
+
+
